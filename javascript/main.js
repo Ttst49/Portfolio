6cefa88137ba693ddbfb928f71f2571e0e4af326
Ttst49 <thibautstachnick@gmail.com>
2022-12-31 10:17:00 +0100

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+const info1 = document.querySelector(".hive")
+const info2 = document.querySelector(".ilo")
+const info3 = document.querySelector(".jad")
+
+
+info1.addEventListener('click',()=>{
+    scrollBy(0,1500)
+})
+
+info2.addEventListener('click',()=>{
+    scrollBy(0,300)
+})
+
+info3.addEventListener('click',()=>{
+    scrollBy(0,1000)
+})
+
