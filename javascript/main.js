c07ddf566c708ad031ca1c317af6af701c56628d
Ttst49 <thibautstachnick@gmail.com>
2022-12-31 14:20:24 +0100

    add animation

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,8 @@
 const info1 = document.querySelector(".hive")
 const info2 = document.querySelector(".ilo")
 const info3 = document.querySelector(".jad")
-
+const scrolling = document.querySelector('.scroll')
+const text = document.querySelector('.base')
 
 info1.addEventListener('click',()=>{
     scrollBy(0,1500)
@@ -15,3 +16,17 @@ info3.addEventListener('click',()=>{
     scrollBy(0,1000)
 })
 
+const observateur = new IntersectionObserver(entries => {
+    entries.forEach(entry=>{
+        if (!entry.isIntersecting){
+            scrolling.classList.add("animation")
+            text.classList.add("animationText")
+        }else{
+            scrolling.classList.remove('animation')
+            text.classList.remove('animationText')
+        }
+
+    })
+})
+
+observateur.observe(document.querySelector('.navbar'))
